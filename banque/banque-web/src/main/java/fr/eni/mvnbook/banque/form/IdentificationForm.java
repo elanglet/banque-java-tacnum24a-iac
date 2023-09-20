f7470d7614a8613831685f6ef5112514cf3dfa8c
Etienne LANGLET <elanglet@eni.fr>
2023-09-20 13:10:55 +0200

    Import initial

---
+++ banque/banque-web/src/main/java/fr/eni/mvnbook/banque/form/IdentificationForm.java
@@ -0,0 +1,24 @@
+package fr.eni.mvnbook.banque.form;
+
+public class IdentificationForm {
+
+	private String identifiant;
+	private String motDePasse;
+
+	public String getIdentifiant() {
+		return identifiant;
+	}
+
+	public void setIdentifiant(String identifiant) {
+		this.identifiant = identifiant;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+}
